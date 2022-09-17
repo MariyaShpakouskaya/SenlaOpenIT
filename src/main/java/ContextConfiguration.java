210c25df558d7f14295b9eeb4a01abf36ceff9d4
Maria <shpackovskaya.ria@yandex.by>
2022-09-17 12:43:41 +0300

    Context

---
+++ src/main/java/ContextConfiguration.java
@@ -0,0 +1,9 @@
+package main.java;
+
+import org.springframework.boot.SpringApplication;
+
+public class ContextConfiguration {
+    public static void main(String[] args) {
+        SpringApplication.run(ContextConfiguration.class, args);
+    }
+}
